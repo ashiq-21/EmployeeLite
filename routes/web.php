ee5da3f644de98cb0b851686f4010b25dee0672f
ashiq-21 <arcashiq@gmail.com>
2024-09-14 01:07:52 +0600

    added route group

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EmployeeController;
 
-Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');
-Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
-Route::post('/employees', [EmployeeController::class, 'store'])->name('employees.store');
-Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
-Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
-Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
+Route::get('/', [EmployeeController::class, 'index'])->name('employees.index');//Show all employees list
+
+Route::prefix('employees')->group(function () {
+    Route::get('/create', [EmployeeController::class, 'create'])->name('employees.create'); // Show create employee form
+
+    Route::post('/', [EmployeeController::class, 'store'])->name('employees.store'); // Store a new employee
+
+    Route::get('/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit'); // Show edit form
+
+    Route::put('/{employee}', [EmployeeController::class, 'update'])->name('employees.update'); // Update an employee
+
+    Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy'); // Delete an employee
+});
 
